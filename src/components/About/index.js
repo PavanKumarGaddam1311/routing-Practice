6e715780af40be5176fe8e124548edec18088cbc
pavankumar <gpavanroy@gmail.com>
2024-10-19 17:00:28 +0000

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here//
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="about-image"
+    />
+    <h1 className="heading-about">About</h1>
+    <p className="description">i love to creat!i am frontend web developer.</p>
+  </div>
+)
+export default About
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="container-notfound">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="NotFound"
+      className="img-notfound"
+    />
+    <h1 className="heading-notfound">Not Found</h1>
+  </div>
+)
+export default NotFound
